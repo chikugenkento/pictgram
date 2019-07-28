8361ead2bfbedca8784682506387eb4a576b93fc
kento chikugen <knt37007@gmail.com>
2019-07-28 15:03:39 +0000

    モデルについて理解

---
+++ pictgram/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+  get 'pages/help'
+end
+
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+end
